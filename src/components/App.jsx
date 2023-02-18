46b3957028a9b0fdf8193e94f81172e994c59c1e
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-02-18 10:51:43 +0200

    Add GlobalStyles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
+import { Feedback } from './Feedback/Feedback';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Feedback />;
 };
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,7 @@
+export const Feedback = () => {
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
+};
